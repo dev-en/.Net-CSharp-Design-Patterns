fb85a28f7b4a3e7dccc9db77df98546b84edace6
Devendra <devendra@nalashaa.com>
2021-02-28 01:00:21 +0530

    Observer design pattern

---
+++ ObsererDesignPattern.Basic/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObsererDesignPattern.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configure Observer pattern
+            ConcreteSubject subj = new ConcreteSubject();
+
+            ConcreteObserver obsA = new ConcreteObserver(subj, "A");
+            ConcreteObserver obsB = new ConcreteObserver(subj, "B");
+            ConcreteObserver obsC = new ConcreteObserver(subj, "C");
+
+            subj.Attach(obsA);
+            subj.Attach(obsB);
+            subj.Attach(obsC);
+
+            // Change subject and notify observers
+            subj.Status = "ABC";
+
+            Console.WriteLine("Enter new state");
+            var state = Console.ReadLine();
+            subj.Status = state;
+
+            Console.WriteLine("Enter new state");
+            state = Console.ReadLine();
+            subj.Status = state;
+
+            Console.WriteLine("Enter new state");
+            state = Console.ReadLine();
+            subj.Status = state;
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Subject
+    {
+        private readonly List<Observer> _observers 
+            = new List<Observer>();
+
+        private string _status;
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in _observers)
+            {
+                o.Update();
+            }
+        }
+
+        // Gets or sets the price
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    Notify();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    class ConcreteSubject : Subject
+    {
+        
+    }
+
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    class ConcreteObserver : Observer
+    {
+        private readonly string _name;
+
+        // Gets or sets subject
+        public ConcreteSubject Subject { get; set; }
+
+        // Constructor
+        public ConcreteObserver(
+          ConcreteSubject subject, string name)
+        {
+            Subject = subject;
+            _name = name;
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("Hi {0}, Subject new state is {1}",
+              _name, Subject.Status);
+        }
+    }
+}
---
+++ ObserverDesignPattern.Bank/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverDesignPattern.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+
+            /// Entry point into console application.
+
+            /// </summary>
+
+            static void Main()
+            {
+                // Create IBM stock and attach investors
+
+                LoanScheme ibm = new LoanScheme("IBM", 120.00);
+                ibm.Attach(new Account("Sorros"));
+                ibm.Attach(new Account("Berkshire"));
+
+                // Fluctuating prices will notify investors
+
+                ibm.Price = 120.10;
+                ibm.Price = 121.00;
+                ibm.Price = 120.50;
+                ibm.Price = 120.75;
+
+                // Wait for user
+
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+
+        /// The 'Subject' abstract class
+
+        /// </summary>
+
+        abstract class Scheme
+
+        {
+            private string _Name;
+            private double _price;
+            private List<IAccount> _accounts = new List<IAccount>();
+
+            // Constructor
+
+            public Scheme(string symbol, double price)
+            {
+                this._Name = symbol;
+                this._price = price;
+            }
+
+            public void Attach(IAccount investor)
+            {
+                _accounts.Add(investor);
+            }
+
+            public void Detach(IAccount investor)
+            {
+                _accounts.Remove(investor);
+            }
+
+            public void Notify()
+            {
+                foreach (IAccount account in _accounts)
+                {
+                    account.Update(this);
+                }
+
+                Console.WriteLine("");
+            }
+
+            // Gets or sets the price
+
+            public double Price
+            {
+                get { return _price; }
+                set
+
+                {
+                    if (_price != value)
+                    {
+                        _price = value;
+                        Notify();
+                    }
+                }
+            }
+
+            // Gets the symbol
+
+            public string Symbol
+            {
+                get { return _Name; }
+            }
+        }
+
+        /// <summary>
+
+        /// The 'ConcreteSubject' class
+
+        /// </summary>
+
+        class LoanScheme : Scheme
+
+        {
+            // Constructor
+
+            public LoanScheme(string symbol, double price)
+              : base(symbol, price)
+            {
+            }
+        }
+
+        /// <summary>
+
+        /// The 'Observer' interface
+
+        /// </summary>
+
+        interface IAccount
+
+        {
+            void Update(Scheme stock);
+        }
+
+        /// <summary>
+
+        /// The 'ConcreteObserver' class
+
+        /// </summary>
+
+        class Account : IAccount
+
+        {
+            private string _name;
+            private Scheme _stock;
+
+            // Constructor
+
+            public Account(string name)
+            {
+                this._name = name;
+            }
+
+            public void Update(Scheme stock)
+            {
+                Console.WriteLine("Notified {0} of {1}'s " +
+                  "change to {2:C}", _name, stock.Symbol, stock.Price);
+            }
+
+            // Gets or sets the stock
+
+            public Scheme Stock
+            {
+                get { return _stock; }
+                set { _stock = value; }
+            }
+        }
+    }
+}
