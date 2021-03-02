1e9cfe081e9bacd3eb85f72050694c27de45c941
Devendra <devendra@nalashaa.com>
2021-03-03 02:27:40 +0530

    Adapter real time implementation

---
+++ Adapter.RealTime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Adapter.RealTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog superDog = new FlyingDog();
+            superDog.Run("SuperDog");
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IFlyObject
+    {
+        void fly(string name);
+    }
+
+    public class Bird : IFlyObject
+    {
+        public virtual void fly(string name)
+        {
+            Console.WriteLine($"{name}, can fly");
+        }
+    }
+
+    public interface IRunObject
+    {
+        void Run(string name);
+    }
+    public class Dog : IRunObject
+    {
+        public virtual void Run(string name)
+        {
+            Console.WriteLine($"{name}, can run");
+        }
+    }
+
+    public class FlyingDog : Dog
+    {
+        private Bird _bird = new Bird();
+        public override void Run(string name)
+        {
+            _bird.fly(name);
+            base.Run(name);
+        }
+    }
+}
